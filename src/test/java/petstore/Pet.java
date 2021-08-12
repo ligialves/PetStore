33b09b2ed9914ad58d953629b276139428fbb883
ligialves <ligia.analista@gmail.com>
2021-08-11 22:32:20 -0300

    Criei o teste para incluir um pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -3,15 +3,40 @@ package petstore;
 
 // Bibliotecas
 
+import org.testng.annotations.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
 //Classe
 public class Pet {
     // Atributo
-    String uri = "https://petstore.swagger.io/v2/pet " // esse endereço pegamos do swagger
+    String uri = "https://petstore.swagger.io/v2/pet " ;// esse endereço pegamos do swagger
 
     // Métodos e funções
-    public String lerJson(){
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
 
-        return null;
+    // Incluir - Create - Post
+    @Test
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+        // dado - quando - então
+        // given - when - then
+        given() // Dado
+                .contentType("application/json") // tipo de informação comum em API REST - antigamente eram "text/xml"
+                .log().all() // registro
+                .body(jsonBody)
+        .when() // Quando
+                .post(uri)
+        .then()
+                .log().all()  // request e response
+                .statusCode(200)
+        ;
     }
 
 }
