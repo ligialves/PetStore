05b8a596f5ea090a3eddbbd64c959ba77c990ad5
ligialves <ligia.analista@gmail.com>
2021-08-11 21:47:06 -0300

    Json

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,6 +1,17 @@
+// Pacote
 package petstore;
 
-public class Pet {
+// Bibliotecas
 
+//Classe
+public class Pet {
+    // Atributo
+    String uri = "https://petstore.swagger.io/v2/pet " // esse endereço pegamos do swagger
+
+    // Métodos e funções
+    public String lerJson(){
+
+        return null;
+    }
 
 }
